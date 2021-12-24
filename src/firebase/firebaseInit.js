e91cad82dc5aeb226d405a420130ecbc1e20c478
Luka Jioshvili <luka.jioshvili20@gmail.com>
2021-12-24 15:15:58 +0400

    hellwalker
    This is fully working CRUD  Invoice Vue 3 application .

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,18 @@
+// VERSION 9
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTioxH6vwzcFZEV0nsmOlRQpvl2IT2FBE",
+    authDomain: "invoice-40706.firebaseapp.com",
+    projectId: "invoice-40706",
+    storageBucket: "invoice-40706.appspot.com",
+    messagingSenderId: "122984197750",
+    appId: "1:122984197750:web:ec224a7c5df23933327957"
+
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import InvoiceView from "../views/InvoiceView";
 const routes = [
   {
     path: '/',
@@ -8,18 +8,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path:'/invoice/:invoiceId',
+    name: 'Invoice',
+    component: InvoiceView
+  },
+
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-})
+});
 
+router.beforeEach((to, from,next)=>{
+  document.title = `${process.env.VUE_APP_TITLE}  -  ${to.name} `
+  next()
+});
 export default router
